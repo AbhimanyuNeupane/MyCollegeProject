879778c7c93be66c13cfcaac92f34eb98a0de3a6
Abhimanyu Neupane <38075836+AbhimanyuNeupane@users.noreply.github.com>
2018-04-15 22:13:38 +0545

    Add files via upload

---
+++ main.c
@@ -0,0 +1,17 @@
+/*WAP to find a character of ASCII character*/
+
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+    int number;
+    printf("Enter a number to know its ASCII character :" );
+    scanf("%d",&number);
+    printf("ASCII character of %d is %c",number,number);
+    getch();
+    return 0;
+
+
+
+
+}
