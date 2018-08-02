75c3e24649831bac1f2e7d128e2c6e5e63872a4d
Abhimanyu Neupane <38075836+AbhimanyuNeupane@users.noreply.github.com>
2018-08-02 15:43:29 +0545

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,25 @@
+//WAP for while loop to print an even  number from 0-100
+//author: Abhimanyu neupane
+//date : june 4 2018
+
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+    int i=0;
+    while(i<=100)
+    {
+        i=i+1;
+        if (i%2==1)
+        {
+           printf("%d\n",i);
+
+        }
+
+
+
+
+    }
+getch();
+
+}
